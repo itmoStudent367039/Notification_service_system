ab327427ac350e213ce6a1a930f6833246df56b9
yestai <igor.adbullin.95@gmail.com>
2023-11-29 18:26:08 +0300

    divide server on auth and user api, add nginx

--- backend/src/main/java/ru/ifmo/backend/authentication/email/EmailService.java
+++
@@ -1,8 +0,0 @@
-package ru.ifmo.backend.authentication.email;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface EmailService {
-    void sendConfirmAccountMessage(String name, String to, String token);
-}
--- backend/src/main/java/ru/ifmo/backend/authentication/exceptions/InvalidEmailException.java
+++
@@ -1,7 +0,0 @@
-package ru.ifmo.backend.authentication.exceptions;
-
-public class InvalidEmailException extends RuntimeException {
-    public InvalidEmailException(String message) {
-        super(message);
-    }
-}

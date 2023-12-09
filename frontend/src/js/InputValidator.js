03e0238cf046861c16adec482eba8ab1593a9632
NelyaShaihutdinova <fohado@mail.ru>
2023-12-09 12:57:09 +0300

    handle errors from server

--- frontend/src/js/InputValidator.js
+++ frontend/src/js/InputValidator.js
@@ -1,21 +1,27 @@
-import {createErrorMessage} from "../index";
+import {createCorrectInput, createErrorMessage} from "../index";
 
 export function checkInputSignUp(response) {
     response.then(result1 => {
         const password = result1.message.password;
         if (password) {
             createErrorMessage("appPasswordSignUp", password);
+        } else {
+            createCorrectInput("appPasswordSignUp");
         }
         const email = result1.message.email;
-        if (email){
+        if (email) {
             createErrorMessage("appMailSignUp", email);
+        } else {
+            createCorrectInput("appMailSignUp");
         }
         const username = result1.message.username;
-        if (username){
+        if (username) {
             createErrorMessage("appNameSignUp", username);
+        } else {
+            createCorrectInput("appNameSignUp");
         }
         const error = result1.message.error;
-        if (error){
+        if (error) {
             createErrorMessage("appPasswordSignUp", error);
         }
     });
@@ -26,13 +32,17 @@ export function checkInputLogIn(response) {
         const password = result.message.password;
         if (password) {
             createErrorMessage("appPasswordLogIn", password);
+        } else {
+            createCorrectInput("appPasswordLogIn")
         }
         const email = result.message.email;
-        if (email){
+        if (email) {
             createErrorMessage("appEmailLogIn", email);
+        } else {
+            createCorrectInput("appEmailLogIn")
         }
         const error = result.message.error;
-        if (error){
+        if (error) {
             createErrorMessage("appPasswordLogIn", error);
         }
     });

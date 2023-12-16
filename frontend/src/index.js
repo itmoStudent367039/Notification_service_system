245bf920681d977c0b5f190fb2e1a467c83cce01
NelyaShaihutdinova <fohado@mail.ru>
2023-12-16 12:41:39 +0300

    try to solve css problem

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './js/components/App';
+import App from './js/App';
 import CorrectInput from "./js/components/CorrectInput";
 import './css/StyleLogIn.css';
 import './js/FormSwitch';
 import './js/Initialization';
+import ErrorMessage from "./js/components/ErrorMessage";
 
-export function createErrorMessage(id, message){
-    const root = ReactDOM.createRoot(document.getElementById(id));
-    root.render(
-        <App message={message}/>
-    );
-}
+// export function createApp(){
+//     const root = ReactDOM.createRoot(document.getElementById(""));
+//     root.render(
+//         <App/>
+//     );
+// }
 
 export function createCorrectInput(id){
     const root = ReactDOM.createRoot(document.getElementById(id));
@@ -20,4 +21,9 @@ export function createCorrectInput(id){
     );
 }
 
-
+export function createErrorMessage(id, message){
+    const root = ReactDOM.createRoot(document.getElementById(id));
+    root.render(
+        <ErrorMessage message={message}/>
+    );
+}
